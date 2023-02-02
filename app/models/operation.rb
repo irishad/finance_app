d944237dd84e899f356a331644fdbae1e2fdefed
ira <irishad1710@gmail.com>
2023-02-02 07:35:41 +0200

    added otype values

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,4 +2,14 @@ class Operation < ApplicationRecord
   belongs_to :category
   belongs_to :family_member
   belongs_to :wallet
+
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :odate, presence: true
+  validates :description, presence: true
+
+  enum otype: {
+    income: 10,
+    expense: 20,
+    transfer: 30
+  }
 end
