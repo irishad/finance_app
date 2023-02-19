1d3430b3cc8aded86bb97aa919bef0c54ab637c4
ira <irishad1710@gmail.com>
2023-02-19 22:11:10 +0200

    add report form

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,11 +1,18 @@
 class ReportsController < ApplicationController
   def report_by_category
+    @wallets = Wallet.all.map { |wal| [wal.name, wal.id]}
+    @otypes = Operation.otypes
   end
 
   def report_by_dates
+    @wallets = Wallet.all.map { |wal| [wal.name, wal.id]}
+    @otypes = Operation.otypes
+    @categories = Category.all.map { |c| [c.name, c.id]}
   end
 
   def report_by_wallets
+    @otypes = Operation.otypes
+    @categories = Category.all.map { |c| [c.name, c.id]}
   end
 
   def result_by_category
