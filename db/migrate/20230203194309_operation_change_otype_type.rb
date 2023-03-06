079b3e2841444015f77465570236b77230ed0362
ira <irishad1710@gmail.com>
2023-03-06 21:13:27 +0200

    change otype migration

--- db/migrate/20230203194309_operation_change_otype_type.rb
+++ db/migrate/20230203194309_operation_change_otype_type.rb
@@ -1,5 +1,5 @@
 class OperationChangeOtypeType < ActiveRecord::Migration[7.0]
   def change
-    change_column(:operations, :otype, :integer)
+    change_column(:operations, :otype, :integer, using: 'otype::integer')
   end
 end
